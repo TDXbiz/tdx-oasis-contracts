880d20de045f9ee8f513a26373cd454c1f2bce13
jnalam-ith <jaya@ith.tech>
2025-09-10 18:13:35 +0530

    fix: add investor on version 3 & deploy

--- scripts/upgrade_profileV3.ts
+++ scripts/upgrade_profileV3.ts
@@ -1,25 +1,17 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 const main = async () => {
     const proxy = "0x487C47491D2224c02324576A83d864FAD7396591"; // proxy address
 
-    // will work regardless of the version .. since upgradeToAndCall funciton abi is same
-    const proxyContract = await ethers.getContractAt("InvestorProfile", proxy);
-
     const factory = await ethers.getContractFactory("InvestorProfileV3");
 
-    const newImplementation = await factory.deploy();
-    await newImplementation.waitForDeployment();
+    const result = await upgrades.upgradeProxy(proxy, factory, {
+        kind: "uups",
+        redeployImplementation: "onchange",
+    });
+    await result.waitForDeployment();
 
-    console.log("New implementation deployed at: ", newImplementation.target);
-
-    const tx = await proxyContract.upgradeToAndCall(
-        newImplementation.target,
-        "0x"
-    );
-
-    await tx.wait(1);
-    console.log("Upgrade transaction sent:", tx.hash);
+    console.log("Upgrade transaction sent:", result.target);
 };
 
 main().catch(console.error);
