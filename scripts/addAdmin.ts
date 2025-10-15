20665c8684a01639c90f2760eb4f57bf437f8791
jnalam-ith <jaya@ith.tech>
2025-10-15 13:38:56 +0530

    chore: fix script

--- scripts/addAdmin.ts
+++ scripts/addAdmin.ts
@@ -1,18 +1,20 @@
-import { InvestorProfile__factory } from "../typechain-types";
+import { ethers } from "hardhat";
 
 const main = async () => {
-    const manager = "0x8BA52eCB5573ACF5265184D80450272bd265604E";
+    const manager = "0xDFD2168901BD0825d48d44e10A8a387A035aaf2F";
 
-    const contractAddress = "";
+    const contractAddress = "0x4c61b87246Be97E8FebFF7d0d7e5a3F9AEd1abE8";
 
-    const profile = InvestorProfile__factory.connect(contractAddress);
+    const profile = await ethers.getContractAt(
+        "InvestorProfileV3",
+        contractAddress
+    );
 
     const role = await profile.DATA_MANAGER();
 
-    const tx = await profile.grantRole(role, manager);
-    await tx.wait(1);
+    const tx = await profile.hasRole(role, manager);
 
-    return tx.hash;
+    return tx;
 };
 
 main().then(console.log);
